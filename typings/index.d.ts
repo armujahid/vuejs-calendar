f7b88b37f8d50dcb159028ee98db8a497a24f532
Abdul Rauf <armujahid@live.com>
2017-06-24 11:45:30 +0500

    Make it Azure Compatible Run it in NODE_ENV=development (by setting this environment variable in Azure App Service App Settings) because this code fails in production mode and azure runs it in production by default

---
+++ typings/index.d.ts
@@ -0,0 +1,6 @@
+/// <reference path="globals/axios/index.d.ts" />
+/// <reference path="globals/moment-timezone/index.d.ts" />
+/// <reference path="globals/moment/index.d.ts" />
+/// <reference path="globals/node/index.d.ts" />
+/// <reference path="globals/vue/index.d.ts" />
+/// <reference path="globals/webpack-merge/index.d.ts" />
